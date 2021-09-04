e925b5a279f0ebed7051f468e22df3d228a66992
rokuosan <85651386+rokuosan@users.noreply.github.com>
2021-09-04 13:46:12 +0900

    Remove sample directory

--- sample/myTools.py
+++
@@ -1,8 +0,0 @@
-# 確認するあれ
-def confirm():
-    check = input("確定しますか?(Y/n): ")
-    if check == 'Y' or check == 'y':
-        state = True
-    elif check == 'n' or check == 'N':
-        state = False
-    return state
